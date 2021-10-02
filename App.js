7de5acf39e4584cbc8d5f9722a6357ba53bbc9e4
Lohitaksh Kaushik <kaushiklohitaksh@gmail.com>
2021-10-02 21:32:40 +0530

    feat(./app.js): a small sentence added to the page

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ export default function App() {
   return (
     <ThemeProvider>
       <View>
-        <Text color="red">Open up App.js to start working on your app!</Text>
+        <Text color="red">
+          Open up App.js to start working on your app! Try not to add any bugs!
+        </Text>
       </View>
     </ThemeProvider>
   );
