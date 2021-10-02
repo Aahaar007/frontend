ca8e1aebf95fdca4a5801ce6f66044cbe8f3b6d5
Lohitaksh Kaushik <kaushiklohitaksh@gmail.com>
2021-10-02 21:14:19 +0530

    fix(./app.js): removed styles import

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { AppRegistry, StyleSheet, View } from "react-native";
+import { AppRegistry, View } from "react-native";
 import { ThemeProvider } from "react-native-magnus";
 import { Text } from "react-native-magnus";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <View style={styles.container}>
+      <View>
         <Text color="red">Open up App.js to start working on your app!</Text>
       </View>
     </ThemeProvider>
