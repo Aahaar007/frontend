90fcfadd41dee423e941facb4a7cb59e8fded1a8
k4droid3 <k4droid3@gmail.com>
2022-02-17 20:15:53 +0530

    create OTP page and add OTP regex

---
+++ containers/signin/OTPForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Div, Text } from "react-native-magnus";
+import OTPInput from "../../components/form/OTPInput";
+import { theme } from "../../styles/theme";
+import { Regex } from "../../constants/Regex";
+
+const OTPForm = (props) => {
+  const [value, setValue] = useState("");
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const submitData = () => {
+    if (!Regex.OTPPattern.test(value)) {
+      console.log("OTP should have 6 numbers");
+    }
+    //TODO: backend OTP test
+    console.table(value);
+  };
+
+  return (
+    <Div {...props}>
+      <OTPInput value={value} setValue={setValue} />
+      <Div alignSelf="center">
+        <Text color={theme.colors.dimGray} textAlign="center">
+          Didn't recieve OTP?
+        </Text>
+        <Text
+          color="white"
+          textDecorLine="underline"
+          fontWeight="bold"
+          textAlign="center"
+          fontSize={17}
+        >
+          Resend OTP
+        </Text>
+      </Div>
+      <Button
+        title="Submit"
+        onPress={submitData}
+        bg="primary"
+        w="100%"
+        h={55}
+        mt={40}
+      >
+        Submit
+      </Button>
+    </Div>
+  );
+};
+
+export default OTPForm;
