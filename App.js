2317acb06707658d53bf7d72324f500e9906da70
k4droid3 <k4droid3@gmail.com>
2022-02-17 21:51:53 +0530

    Fix typo

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import SignInScreen from "./screens/SignInScreen";
-import OTPScreen from "./screens/OtpScreen";
+import OTPScreen from "./screens/OTPScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
