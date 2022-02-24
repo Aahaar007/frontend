972e73e8fb251be7849a9a2f57f76c959a7ea01b
Avijit Pandey <avijeetpandey87@gmail.com>
2022-02-24 15:49:50 +0530

    feat(root): add rtq query slice and aahaar user api hooks
    setup rtq query slice for aahaar api collection, add query hooks for the user management apis
    
    closes #39

--- store.js
+++ store.js
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
+import { aahaarApi } from "./services/aahaar";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    [aahaarApi.reducerPath]: aahaarApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(aahaarApi.middleware),
 });
