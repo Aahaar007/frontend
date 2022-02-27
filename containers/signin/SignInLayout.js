95c77e21f12b199849f9ff1bbb6e5f6fbc118a18
k4droid3 <k4droid3@gmail.com>
2022-02-27 20:16:53 +0530

    refactor(signin/signinlayout): refactor signInLayout
    refactor signInLayout code similar to SignUpLayout code
    
    closes #31

--- containers/signin/SignInLayout.js
+++ containers/signin/SignInLayout.js
@@ -1,13 +1,97 @@
-import React from "react";
-
-import { Div } from "react-native-magnus";
+import React, { useState, useCallback, useEffect } from "react";
+import { Div, Button, Text } from "react-native-magnus";
+import { useNavigation } from "@react-navigation/core";
+import PhoneInput from "../../components/form/PhoneInput";
 import HeroSignIn from "./HeroSignIn";
-import SignInForm from "./SignInForm";
+import { useForm } from "react-hook-form";
+import { isValidPhoneNumber } from "libphonenumber-js";
+import OTPForm from "../../components/form/OTP/OTPForm";
+
 const SignInLayout = () => {
+  const [userData, setUserData] = useState({});
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    setValue,
+    register,
+  } = useForm();
+  const navigator = useNavigation();
+  const modifyData = useCallback(
+    (val) => {
+      console.log("USER:", userData, "VAL: ", val);
+      const temp = { ...userData, ...val };
+
+      setUserData(temp);
+    },
+    [userData]
+  );
+
+  //debug log
+  useEffect(() => {
+    console.log("userData: ", userData);
+  }, [userData]);
+
+  const submitData = (data) => {
+    if (data["phone"]) {
+      if (isValidPhoneNumber(data["phone"], userData.code)) {
+        modifyData(data);
+      } else {
+        setError("phone", {
+          type: "manual",
+          message: "Enter a valid phone number",
+        });
+      }
+    }
+    console.log("data", data);
+  };
+
+  const getComponent = () => {
+    if (Object.entries(userData).length < 2) {
+      //phone input component
+      return (
+        <PhoneInput
+          mt={100}
+          setData={modifyData}
+          control={control}
+          errors={errors}
+        />
+      );
+    } else {
+      return <OTPForm register={register} phone={userData?.phone} />;
+    }
+  };
+
   return (
     <Div>
       <HeroSignIn mt={157} />
-      <SignInForm mt={100} />
+      {getComponent()}
+      <Button
+        title="Submit"
+        onPress={handleSubmit((data) => submitData(data))}
+        bg="primary"
+        w="100%"
+        h={55}
+        mt={40}
+      >
+        Submit
+      </Button>
+      <Div row mt="50%" ml="auto" position="relative">
+        <Text color="dimGray">No account? </Text>
+        <Button p={0} bg="transparent">
+          <Text
+            color="white"
+            textDecorationLine="underline"
+            fontWeight="bold"
+            onPress={() => {
+              navigator.navigate("SignUp");
+            }}
+          >
+            Sign Up
+          </Text>
+        </Button>
+      </Div>
     </Div>
   );
 };
